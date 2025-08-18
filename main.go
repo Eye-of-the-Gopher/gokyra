983fc1e912dd900f41674320271c399c5db388ca
Noufal Ibrahim <noufal@hamon.in>
2025-08-19 00:38:52 +0530

    Implements pakfile extractor

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "log/slog"
+import (
+	"fmt"
+	"log/slog"
+	"os"
+	"path/filepath"
+)
+
+func setLogger() {
+	logger := slog.New(slog.NewTextHandler(
+		os.Stderr,
+		&slog.HandlerOptions{Level: slog.LevelInfo}))
+	slog.SetDefault(logger)
+
+}
 
 func main() {
+	setLogger()
 	slog.Info("Starting program")
+	unpakked, err := extractPakFile(os.Args[1])
+	slog.Info("Parsed PAK file")
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		unpakkedDir := fmt.Sprintf("./%s_files", filepath.Base(os.Args[1]))
+		writePakData(unpakked, unpakkedDir)
+		slog.Info("Written entries to", "dir", unpakkedDir)
+	}
+
 }
--- pak.go
+++ pak.go
@@ -10,22 +10,22 @@ import (
 )
 
 type PakData struct {
-	fname string
-	data  []byte
+	name string
+	data []byte
 }
 
 func extractPakFile(pakfile string) ([]PakData, error) {
-	fnames := []string{}
+	filenames := []string{}
 	offsets := []uint32{}
 	slog.Debug("extracting Pakfile", "name", pakfile)
 
 	f, err := os.Open(pakfile)
 	if err != nil {
-		return nil, fmt.Errorf("can't open file: %w", err)
+		return nil, fmt.Errorf("cannot open file: %w", err)
 	}
 	defer f.Close()
 
-	for {
+	for { // The header will end when have some offsets/files and when we read past the first offset (i.e. when the data begins)
 		if len(offsets) != 0 {
 			firstOffset := offsets[0]
 			t, err := f.Seek(0, io.SeekCurrent)
@@ -41,14 +41,14 @@ func extractPakFile(pakfile string) ([]PakData, error) {
 
 		}
 
-		var data uint32
-		err = binary.Read(f, binary.LittleEndian, &data)
+		var offset uint32
+		err = binary.Read(f, binary.LittleEndian, &offset)
 		if err != nil {
 			return nil, fmt.Errorf("unexpected short read while looking for offset: %w", err)
 		}
 
-		slog.Debug(fmt.Sprintf("Offset is %d", data))
-		offsets = append(offsets, data)
+		slog.Debug("Offset obtained", "offset", offset)
+		offsets = append(offsets, offset)
 
 		var fnamechar byte
 
@@ -70,20 +70,20 @@ func extractPakFile(pakfile string) ([]PakData, error) {
 			fnamechars = append(fnamechars, fnamechar)
 		}
 		fname := string(fnamechars)
-		fnames = append(fnames, fname)
-		slog.Debug(fmt.Sprintf("Fname is %s", fname))
+		filenames = append(filenames, fname)
+		slog.Debug("Filename obtained ", "name", fname)
 	}
 
-	ret := make([]PakData, 0)
+	ret := make([]PakData, len(offsets))
 	stat, err := f.Stat()
 	if err != nil {
-		return nil, fmt.Errorf("couldn't stat file: %w", err)
+		return nil, fmt.Errorf("could not stat file: %w", err)
 	}
 
 	fileLimit := uint64(stat.Size())
 
 	for i := range len(offsets) {
-		fname := fnames[i]
+		filename := filenames[i]
 		start := uint64(offsets[i])
 		var end uint64
 		if i+1 == len(offsets) {
@@ -91,23 +91,24 @@ func extractPakFile(pakfile string) ([]PakData, error) {
 		} else {
 			end = uint64(offsets[i+1]) - 1
 		}
-		slog.Debug("Reading", "fname", fname, "From", start, "To", end)
+		slog.Debug("Reading", "file", filename, "From", start, "To", end)
 		_, err = f.Seek(int64(start), io.SeekStart)
 		if err != nil {
-			return nil, fmt.Errorf("couldn't extract %s from %s at offset %d : %w", fname, pakfile, start, err)
+			return nil, fmt.Errorf("could not extract %s from %s at offset %d: %w", filename, pakfile, start, err)
 		}
 		data := make([]byte, end-start)
 		n, err := f.Read(data)
 		if err != nil || n != len(data) {
-			return nil, fmt.Errorf("short read while unpacking %s (position : %d): %w", fname, i, err)
+			return nil, fmt.Errorf("short read while unpacking %s (position : %d): %w", filename, i, err)
 
 		}
 		t := PakData{
-			fname: fname,
-			data:  data,
+			name: filename,
+			data: data,
 		}
-		ret = append(ret, t)
+		ret[i] = t
 	}
+	slog.Info("contents extracted")
 	return ret, nil
 }
 
@@ -116,7 +117,7 @@ func writePakData(data []PakData, basedir string) {
 
 	for i := range data {
 		pakFile := data[i]
-		name := pakFile.fname
+		name := pakFile.name
 		contents := pakFile.data
 		opfile := filepath.Join(basedir, name)
 		f, err := os.Create(opfile)
