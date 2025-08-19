ddc8387d0fee83224754dc8f7cd2921e31224b72
Noufal Ibrahim <noufal@hamon.in>
2025-08-20 01:17:14 +0530

    Implements rules 1 and 4

--- cmp.go
+++ cmp.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/binary"
 	"fmt"
+	"log/slog"
 )
 
 type CMPHeader struct {
@@ -30,8 +31,56 @@ func (h CMPHeader) String() string {
 		h.fileSize, h.compressionType, h.uncompressedSize, h.paletteSize)
 }
 
-func parseCmpBody(header CMPHeader, data []byte) ([]byte, error) {
-	return []byte{}, nil
+func parseCmpBody(header CMPHeader, input []byte) ([]byte, error) {
+	output := make([]byte, header.uncompressedSize)
+	inputPos := 0
+	outputPos := 0
+
+	for {
+		current := input[inputPos]
+		if current == 0x80 {
+			slog.Debug("End of stream")
+			break
+		} else if current&0x80 == 0 {
+			slog.Debug("Command 2 encountered")
+			break
+		} else if current == 0xfe {
+			slog.Debug("Command 4 encountered")
+			inputPos += 1 // Go to count
+			count := binary.LittleEndian.Uint16(input[inputPos : inputPos+2])
+			inputPos += 2 // Go to value
+			value := input[inputPos]
+			inputPos += 1 // Go to next command
+			pattern := fmt.Sprintf("%08b", value)
+			slog.Debug("Copying bytes to output", "count", count, "value", value, "in binary", pattern)
+			for range count {
+				output[outputPos] = value
+				outputPos += 1
+			}
+		} else if current == 0xff {
+			slog.Debug("Command 5 encountered")
+			break
+		} else if current&0xc0 == 0x80 {
+			slog.Debug("Command 1")
+			pattern := fmt.Sprintf("%08b", current)
+			count := current & 0x3f
+			slog.Debug("Copying bytes to output", "count", count, "pattern", pattern)
+			inputPos += 1 // Go to next command
+			for range count {
+				output[outputPos] = input[inputPos]
+				inputPos += 1
+				outputPos += 1
+			}
+		} else if current&0xc0 == 0xc0 {
+			slog.Debug("Command 3")
+			break
+		} else {
+			slog.Error("Corrupt file. This shouldn't happen")
+			break
+		}
+	}
+
+	return output, nil
 }
 
 func decodeCmp(filename string, fileContents []byte) {
