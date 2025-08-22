a2efad91c393da886d46c967190343748237ccdb
Noufal Ibrahim <noufal@hamon.in>
2025-08-22 18:08:24 +0530

    Moves setupLogging to utils

--- decodecmp.go
+++ decodecmp.go
@@ -2,39 +2,14 @@ package main
 
 import (
 	"fmt"
-	"io"
-	"log/slog"
 	"os"
 )
 
-func setupLogging() {
-	logFile, err := os.OpenFile("op.log", os.O_CREATE|os.O_WRONLY, 0666)
-	if err != nil {
-		errorAndExit("Couldnt open log file")
-		// handle error
-	}
-
-	multiWriter := io.MultiWriter(os.Stderr, logFile)
-
-	logger := slog.New(slog.NewTextHandler(
-		multiWriter,
-		&slog.HandlerOptions{
-			Level: slog.LevelDebug,
-			ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
-				if a.Key == slog.TimeKey {
-					return slog.Attr{}
-				}
-				return a
-			},
-		}))
-	slog.SetDefault(logger)
-}
-
 func main() {
+	setupLogging("cmp-decode.log")
 	fmt.Println("./decodecmp dataFile paletteFile ouputImageFile")
 	if len(os.Args) != 4 {
 		errorAndExit("Usage : ./decodecmp dataFile paletteFile ouputImageFile")
-	setupLogging()
 	}
 
 	dataFile := os.Args[1]
