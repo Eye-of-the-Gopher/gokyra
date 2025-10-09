ad6379dc78719677967a0a2b271358220239a691
Noufal Ibrahim <noufal@hamon.in>
2025-10-09 19:24:30 +0530

    Cross fade implemented

--- pkg/engine/intro.go
+++ pkg/engine/intro.go
@@ -1,6 +1,7 @@
 package engine
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/hajimehoshi/ebiten/v2"
@@ -55,10 +56,11 @@ func (g *Game) PlayScene(scenes []ImageStage) {
 }
 
 type IntroManager struct {
-	stageIndex    int
-	stages        []ImageStage
-	fading        bool
-	fadingCounter float32
+	stageIndex int
+	stages     []ImageStage
+	fading     bool
+	fadeStart  time.Time
+	fadeAlpha  float64
 }
 
 func (i *IntroManager) Update(game *Game) error {
@@ -70,10 +72,12 @@ func (i *IntroManager) Update(game *Game) error {
 		stage.startedAt = time.Now()
 		EngineLogger.Debug("Starting stage", "name", stage.name, "at", stage.startedAt)
 		if stage.track != nil { // Stage has a track
+			EngineLogger.Debug("Staget track is not nil")
 			if game.currentTrack == nil { // But nothing is playing
 				audioPlayer, err := stage.track.GetEbintenPlayer(game.audioContext) // Get ready to play
 				if err == nil {
 					game.currentTrack = audioPlayer
+					game.currentTrack.Play()
 				}
 			} else if game.currentTrack.IsPlaying() { // There's something already playing
 				err := game.currentTrack.Close() // Stop it
@@ -83,25 +87,32 @@ func (i *IntroManager) Update(game *Game) error {
 				audioPlayer, err := stage.track.GetEbintenPlayer(game.audioContext) // and create a new player
 				if err == nil {
 					game.currentTrack = audioPlayer
+					game.currentTrack.Play()
 				}
 			}
-		} else { // The stage doesn't have a track
-			if game.currentTrack != nil { // If there's something playing
-				err := game.currentTrack.Close() // Stop it
-				if err != nil {
-					EngineLogger.Warn("Couldn't stop current track ", "reason", err)
-				}
-			}
-		}
+		} // else { // The stage doesn't have a track
+		// 	EngineLogger.Debug("Staget track is nil")
+		// 	if game.currentTrack != nil { // If there's something playing
+		// 		err := game.currentTrack.Close() // Stop it
+		// 		if err != nil {
+		// 			EngineLogger.Warn("Couldn't stop current track ", "reason", err)
+		// 		}
+		// 	}
+		// }
 	} else {
-		if time.Since(stage.startedAt) > stage.fadeStart {
+		if (time.Since(stage.startedAt) > stage.fadeStart) && i.fading == false {
 			EngineLogger.Debug("Starting fade", "name", stage.name, "at", stage.fadeStart)
 			i.fading = true
-			i.fadingCounter = 0
+			i.fadeStart = time.Now()
+			i.fadeAlpha = 0
+		}
+		if i.fading {
+			i.fadeAlpha = time.Since(i.fadeStart).Seconds() / (float64(stage.displayTime.Seconds()) - float64(stage.fadeStart.Seconds()))
 		}
 
 		if time.Since(stage.startedAt) > stage.displayTime {
 			i.stageIndex++
+			i.fading = false
 			EngineLogger.Debug("Going to next page")
 		}
 	}
@@ -109,33 +120,27 @@ func (i *IntroManager) Update(game *Game) error {
 }
 
 func (i *IntroManager) Draw(screen *ebiten.Image, game *Game) {
-	// EngineLogger.Debug("Game is Drawing Intro")
 	stage := i.stages[i.stageIndex]
 	img, _ := stage.image.GetEbitenImage()
 	screen.DrawImage(img, nil)
-	if game.currentTrack != nil && !game.currentTrack.IsPlaying() {
-		game.currentTrack.Play()
-	}
 	if i.fading {
 		nStage := i.stages[i.stageIndex+1]
 		nImg, _ := nStage.image.GetEbitenImage()
 		op := &ebiten.DrawImageOptions{}
-		op.ColorScale.ScaleAlpha(float32(i.fadingCounter) / 10) // 0.0 to 1.0
-		i.fadingCounter = +0.05
+		fmt.Println(i.fadeAlpha)
+		op.ColorScale.ScaleAlpha(float32(i.fadeAlpha)) // 0.0 to 1.0
 		screen.DrawImage(nImg, op)
-		if i.fadingCounter > 10 {
-			i.fading = false
-		}
 	}
 
 }
 
 func NewIntroManager(scenes []ImageStage) *IntroManager {
 	ret := IntroManager{
-		fadingCounter: 0,
-		fading:        false,
-		stages:        scenes,
-		stageIndex:    0,
+		fadeAlpha:  0,
+		fadeStart:  time.Time{},
+		fading:     false,
+		stages:     scenes,
+		stageIndex: 0,
 	}
 	return &ret
 }
