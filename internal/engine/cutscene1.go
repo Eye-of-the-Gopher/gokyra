4998b742581dc6f64494432fe6c08a7aba804862
Noufal Ibrahim <noufal@hamon.in>
2025-12-10 14:14:28 +0530

    Uses pixel positions and let's ebiten do the scaling

--- internal/engine/cutscene1.go
+++ internal/engine/cutscene1.go
@@ -29,7 +29,7 @@ func NewScene1(c *CutSceneManager) (*Scene1, error) {
 		return nil, err
 	}
 
-	srcRect := image.Rect(0, 0, 1280, 131)
+	srcRect := image.Rect(0, 0, 320, 31)
 	textSpriteImage, err := textSprite.GetEbitenImage()
 	if err != nil {
 		return nil, err
@@ -50,9 +50,13 @@ func NewScene1(c *CutSceneManager) (*Scene1, error) {
 	// These rect dimentions were arrived at by
 	// experimentation. 513 and 1023 seem to make sense. (512,
 	// 1024) but not sure about the heights
-	towerShaftRect := image.Rect(513, 417, 1023, 671)
-	windowRect := image.Rect(0, 0, 510, 572)
-	mageCircleRect := image.Rect(514, 0, 1023, 414)
+
+	// towerShaftRect := image.Rect(513, 417, 1023, 671)
+	towerShaftRect := image.Rect(128, 104, 256, 167)
+
+	windowRect := image.Rect(0, 0, 128, 143) //
+
+	mageCircleRect := image.Rect(128, 0, 256, 104)
 
 	return &Scene1{
 		towerSprite:      towrmageImage.SubImage(towerShaftRect),
@@ -69,7 +73,8 @@ func (c *CutSceneManager) Scene1Update(game *Game) (bool, error) {
 	if c.subtitle == nil {
 		c.subtitle = c.scene1.text1.(*ebiten.Image)
 	}
-	if c.scene1.scrollOffset <= 650 {
+
+	if c.scene1.scrollOffset <= 63*3+16 { // 63 is the height of the shaft segment. We move 3 segments down + 16 pixels for the extra in the window
 		c.scene1.scrollOffset += 1
 	}
 
@@ -80,34 +85,37 @@ func (c *CutSceneManager) Scene1Draw(screen *ebiten.Image, game *Game) {
 	if c.scene1.mageCircleSprite != nil {
 		op := &ebiten.DrawImageOptions{}
 		t := c.scene1.mageCircleSprite.(*ebiten.Image)
-		op.GeoM.Translate(357, 0)
+		op.GeoM.Translate(96, 30) // 16 pixels for the extra height of the window sprite and 14 for the black part on top of the mages
 		screen.DrawImage(t, op)
 	}
 
-	if c.scene1.towerSprite != nil {
+	if c.scene1.towerSprite != nil { // Tower shaft sprite height is 63. We have 4. 1 (top), 2, 3, and 4
 		op := &ebiten.DrawImageOptions{}
 		// op.GeoM.Translate(257, 200)
 		t := c.scene1.towerSprite.(*ebiten.Image)
-		op.GeoM.Translate(357, 415+float64(c.scene1.scrollOffset))
+		op.GeoM.Translate(96, 126+float64(c.scene1.scrollOffset)) // Segment 4
 		screen.DrawImage(t, op)
 		op.GeoM.Reset()
-		op.GeoM.Translate(357, 163+float64(c.scene1.scrollOffset))
+		op.GeoM.Translate(96, 63+float64(c.scene1.scrollOffset)) // Segment 3
 		screen.DrawImage(t, op)
 		op.GeoM.Reset()
-		op.GeoM.Translate(357, -89+float64(c.scene1.scrollOffset))
+		op.GeoM.Translate(96, 0+float64(c.scene1.scrollOffset)) // Segment 2
+		screen.DrawImage(t, op)
+		op.GeoM.Reset()
+		op.GeoM.Translate(96, -63+float64(c.scene1.scrollOffset)) // Segment 1
 		screen.DrawImage(t, op)
 
 	}
 
 	if c.scene1.windowSprite != nil {
-		op := &ebiten.DrawImageOptions{}
-		op.GeoM.Translate(357, -661+float64(c.scene1.scrollOffset))
+		op := &ebiten.DrawImageOptions{} // 143 is the height of the window. 63 for shaft segment 1
+		op.GeoM.Translate(96, -143-63+float64(c.scene1.scrollOffset))
 		t := c.scene1.windowSprite.(*ebiten.Image)
 		screen.DrawImage(t, op)
 	}
 	if c.subtitle != nil {
 		op := &ebiten.DrawImageOptions{}
-		op.GeoM.Translate(0, 669)
+		op.GeoM.Translate(0, 169)
 		screen.DrawImage(c.subtitle, op)
 	}
 
